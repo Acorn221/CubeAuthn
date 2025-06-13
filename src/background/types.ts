52545957b534f10c43505c2c9304e3b0cb6af53b
James Arnott <james@rno.tt>
2025-06-13 13:53:49 +0100

    feat: created SerializablePublicKeyCredential type

--- src/background/types.ts
+++ src/background/types.ts
@@ -33,3 +33,36 @@ export interface OutboundMessages {
 		response: { num: string }; // using string bc it's a big number
 	};
 }
+
+export type SerializablePublicKeyCredential = {
+    id: string;
+    type: string;
+    rawId: string; // ArrayBuffer -> base64
+    authenticatorAttachment: string | null;
+    response: {
+        clientDataJSON: string; // ArrayBuffer -> base64
+        // For AuthenticatorAttestationResponse
+        attestationObject?: string; // ArrayBuffer -> base64
+        authenticatorData?: string; // ArrayBuffer -> base64
+        publicKey?: string | null; // ArrayBuffer -> base64
+        publicKeyAlgorithm?: number;
+        transports?: string[];
+        // For AuthenticatorAssertionResponse
+        signature?: string; // ArrayBuffer -> base64
+        userHandle?: string | null; // ArrayBuffer -> base64
+    };
+    clientExtensionResults: {
+        appid?: boolean;
+        credProps?: {
+            rk?: boolean;
+        };
+        hmacCreateSecret?: boolean;
+        prf?: {
+            enabled?: boolean;
+            results?: {
+                first: string; // BufferSource -> base64
+                second?: string; // BufferSource -> base64
+            };
+        };
+    };
+};
