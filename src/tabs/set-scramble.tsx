5db69297d24e41e65119c2ab7596ee23a7b62adf
James Arnott <james@rno.tt>
2025-06-15 16:20:26 +0100

    WIP: create set scramble

---
+++ src/tabs/set-scramble.tsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+const SetScramble = () => {
+	return (
+		<div className="min-h-screen h-screen w-screen bg-black flex justify-center align-middle">
+			<div className="m-auto">hi</div>
+		</div>
+	)
+}
+
+export default SetScramble;
