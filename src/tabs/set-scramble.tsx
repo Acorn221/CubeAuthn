5f27b7d8165115a90ff8c299d3879def1b82faa0
James Arnott <james@rno.tt>
2025-06-18 12:18:28 +0100

    chore: fix build

--- src/tabs/set-scramble.tsx
+++ src/tabs/set-scramble.tsx
@@ -1,4 +1,4 @@
-import "./set-scramble/styles.css"
+import "@/tabs/set-scramble/styles.css"
 
 import RubiksCubeIcon from "@/components/apple-style/rubiks-cube-icon"
 import { BTCube } from "gan-i3-356-bluetooth"
